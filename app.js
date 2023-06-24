56c87b9f5718f671a6c3043d197eb208838e964d
Kidus Zegeye <kmz25@cornell.edu>
2023-06-24 19:33:11 -0400

    Add autodeploy

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const path = require("path");
-gi;
 const app = express();
 const port = 3000;
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 app.get("/api/", (req, res) => {
   res.send("Hello World!");
